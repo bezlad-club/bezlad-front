5cd7bc19f902bdd4c8dc8a809fc09ec36f701ce5
bluestack-lab <314490395+bluestack-lab@users.noreply.github.com>
2026-09-08 11:43:40 +0200

    Log messages in prev/dev env if no CHAT_ / BOT_ID specified

--- src/app/(frontend)/api/telegram/route.ts
+++ src/app/(frontend)/api/telegram/route.ts
@@ -1,14 +1,25 @@
 import axios from "axios";
 import { NextRequest, NextResponse } from "next/server";
 
-const BOT_ID = process.env.TELEGRAM_BOT_ID || "";
-
+const BOT_ID = process.env.TELEGRAM_BOT_ID;
 const CHAT_ID = process.env.TELEGRAM_CHAT_ID;
+
+const isDevOrPreview =
+  process.env.NODE_ENV === "development" ||
+  process.env.NEXT_PUBLIC_VERCEL_ENV === "preview" ||
+  process.env.NEXT_PUBLIC_VERCEL_ENV === "development";
 
 export async function POST(request: NextRequest) {
   if (request.method === "POST") {
+    const data = await request.json();
+
+    if ((!BOT_ID || !CHAT_ID) && isDevOrPreview) {
+      console.warn("TELEGRAM_BOT_ID or TELEGRAM_CHAT_ID is not set");
+      console.log("Telegram message:", data);
+      return NextResponse.json({ message: "Message logged to console" });
+    }
+
     try {
-      const data = await request.json();
       await axios.post(`https://api.telegram.org/bot${BOT_ID}/sendMessage`, {
         chat_id: CHAT_ID,
         parse_mode: "html",
